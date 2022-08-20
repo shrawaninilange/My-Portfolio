40f739ea87b8eccc1c2ebb9b13de0635cf47b56b
Shravani Nilange <shrawaninilange1999@gmail.com>
2022-08-20 21:46:46 +0530

    Portfolio

---
+++ Shrawani-Portfolio/src/Utils/Projects.js
@@ -0,0 +1,67 @@
+export const projects = [
+	{
+		img: 'https://i.ibb.co/KG8psBp/cric.png',
+		title: 'Cricbuzz Clone Team-Project',
+		des: 'Cricbuzz provides All News, Video clips related to cricket.Features- Featuring Cricket Videos and Cricket News Purchasing Subscription Plans Live Score Page Login and OTP page Payment Page .',
+		demo: '',
+		live: 'https://dhawal07.github.io/cricbuzz/',
+		gitHub: 'https://github.com/shrawaninilange/cricbuzz',
+		technologies: ['JavaScript,',"HTML," ,'CSS'],
+	},
+	// {
+	// 	img: 'https://i.ibb.co/chch7pg/Screenshot-2022-03-15-003420-2.png',
+	// 	title: 'Bobbibrowncosmetic Clone',
+	// 	des: 'Bobbi Brown Cosmetics is a global prestige beauty brand that empowers women to embrace and enhance their individual beauty.Bobbi Brown Makeup Products| Luxury Makeup Brand - Select Citywalk.',
+	// 	demo: '',
+	// 	live: 'bib-bobrown-cosmetic.netlify.app/',
+	// 	gitHub: 'https://github.com/shrawaninilange/bob-bibrown-cosmetic',
+	// 	technologies: ['JavaScript,',"HTML," ,'CSS',"React.js" , "Redux"],
+	// },
+	{
+		img: 'https://i.ibb.co/YRcYWT3/bob.png',
+		title: 'Bobbibrowncosmetic Clone Solo-Project',
+		des: 'Bobbi Brown Cosmetics is a global prestige beauty brand that empowers women to embrace and enhance their individual beauty.Bobbi Brown Makeup Products| Luxury Makeup Brand - Select Citywalk.',
+		demo: '',
+		live: 'https://bib-bobrown-cosmetic.netlify.app/',
+		gitHub: 'https://github.com/shrawaninilange/bob-bibrown-cosmetic',
+		technologies: [ 'HTML,', 'CSS,', 'javascript,',"React.js," , "Redux"],
+	},
+	{
+		img: 'https://i.ibb.co/Jvy4rtg/fake.png',
+		title: 'FakeApp Solo-Project',
+		des: 'FakeApp Is E-Commerce Website .Made React-Redux ,Css Other Many Libraries .It Has Sorting Filtering Functionality.',
+		demo: '',
+		live: 'https://ephemeral-yeot-a504e6.netlify.app/',
+		gitHub: 'https://github.com/shrawaninilange/Fake-app',
+		technologies: [ 'HTML,', 'CSS,', 'javascript,',"React.js," , "Redux"],
+	},
+	// {
+		// 	img: 'https://camo.githubusercontent.com/b7f4aa8c34ddf776dd19b78dc3488456470a73e5026f48784e9e753601468802/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f4c62507030307771454537386c6f68584d372f67697068792e676966',
+		// 	title: 'Clone of Fitness Blender',
+		// 	des: 'This is a clone of the Fitness Blender website. The clone is complete with a Login page, Sign-in page, Shopping Cart, Wishlist and Calendar Scheduling feature. Following is the link to the clone of the website.',
+		// 	demo: '',
+		// 	live: 'https://anshusagar.github.io/FitnessBlender/',
+		// 	gitHub: 'https://github.com/Anshusagar/FitnessBlender.git',
+		// 	technologies: ['HTML', 'CSS', 'javaScript'],
+		// },
+		// {
+			// 	img: 'https://github.com/mandarsatam/Mailchimp-Clone/blob/master/Snips/Screenshot%20(79).png?raw=true',
+			// 	title: 'Mailchimp Clone',
+			// 	des: 'This is a static clone of a website called Mailchimp. This clone includes landing, pricing, our story, and the resources page.',
+			// 	demo: '',
+			// 	live: 'https://mandarsatam.github.io/Mailchimp-Clone/',
+	// 	gitHub: 'https://github.com/mandarsatam/Mailchimp-Clone',
+	// 	technologies: ['HTML', 'CSS', 'javaScript'],
+	// },
+	{
+		img: 'https://i.ibb.co/31ckRYf/licious.png',
+		title: 'Licious-Clone  Team-Project ',
+		des: 'Licious is India first D2C Unicorn headquartered in Bengaluru, Karnataka, India.The company operates on a farm-to-fork model owning the entire back-end supply chain and cold chain .',
+		demo: '',
+		live: 'https://licious-flow.netlify.app/', // deployed link
+		gitHub: 'https://github.com/deshmukh612/-flowing-basketball-1538',
+		technologies: [ 'HTML,', 'CSS,', 'javaScript,' ,"React.js," , "Redux"],
+	},
+];
+
+ 
